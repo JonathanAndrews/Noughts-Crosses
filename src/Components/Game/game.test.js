fcd6e0830bebf55f6b5dd845776815309189418c
Jonathan Andrews <jonnyandrews.jma@gmail.com>
2018-11-04 17:58:12 +0000

    checking the winner is rendered correctly

--- src/Components/Game/game.test.js
+++ src/Components/Game/game.test.js
@@ -15,4 +15,19 @@ it('renders game status correctly', () => {
   button.simulate('click')
   const secondPlayer = wrapper.find('div.game-info').children().first().text()
   expect(secondPlayer).toEqual('Next player: O')
+
+  const turn2 = wrapper.find('button.square').at(1)
+  turn2.simulate('click')
+  
+  const turn3 = wrapper.find('button.square').at(4)
+  turn3.simulate('click')
+  
+  const turn4 = wrapper.find('button.square').at(5)
+  turn4.simulate('click')
+  
+  const turn5 = wrapper.find('button.square').at(8)
+  turn5.simulate('click')
+
+  const winner = wrapper.find('div.game-info').children().first().text()
+  expect(winner).toEqual('Winner: X')
 })
